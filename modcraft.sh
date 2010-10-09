6e44fe4217f9e16b78c591526a4e28a9c2757bb4
Bryce Mecum <petridish@gmail.com>
2010-10-08 16:55:58 -0800

    First attempt

---
+++ modcraft.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+# modcraft.sh is a painfully simple Minecraft mod installer.
+#
+# PREREQUISITES
+#
+#   modcraft.sh assumes the following things:
+#
+#     1. You are using OSX
+#     2. The executable `jar` is in your PATH
+#      
+#  
+# INSTALLATION
+#
+#   Place this anywhere you like so long as it's in your PATH. Then run:
+#   
+#     `chmod +x modcraft.sh`
+#
+#   to make modcraft.sh executable
+#
+#
+# USAGE
+#   
+#   Install mods by making a folder called 'mods':
+#   
+#     ~/Library/Application Support/minecraft/mods/{MOD_NAME}
+#   
+#   and placing any .class files there. I prefer to make subdirectories 
+#   for each of the mods so I can remember what they do.
+
+#   For example, to install the Better Grass mod, I must place 
+#   the my.class from the mod into  :
+#     
+#     ~/Library/Application Support/minecraft/mods/bettergrass3
+
+set -e
+
+minecraft="/Users/bryce/Library/Application Support/minecraft" 
+
+if [ ! -d "$minecraft" ]; then
+  echo "[ERROR] Couldn't find Minecraft folder."
+  exit 1
+fi
+
+if [ ! -f "$minecraft/bin/minecraft.jar" ]; then
+  echo "[ERROR] Couldn't find minecraft.jar"
+fi
+  
+# Unpack jar
+echo "1. Creating temporary directory..."
+if [ -d "$minecraft/tmp" ]; then
+  rm -rf "$minecraft/tmp"
+fi
+
+mkdir "$minecraft/tmp"
+cd "$minecraft/tmp"
+
+echo "2. Extracting minecraft.jar into temporary directory..."
+jar xf "$minecraft/bin/minecraft.jar"
+
+# Copy in all mods
+# Find .class files recursively and copy in each one
+echo "3. Finding mods..."
+
+cd "$minecraft/mods"
+
+for mod in "`find . -name *.class -print0`"; do
+  echo "\tFound modification: $mod"
+  cp "$mod" "$minecraft/tmp"
+done
+
+# Delete MOJANGs from extracted minecraft.jar
+if [ -f "$minecraft/tmp/META-INF/MOJANG_C.DSA" ]; then
+  rm "$minecraft/tmp/META-INF/MOJANG_C.DSA"
+fi
+  
+if [ -f "$minecraft/tmp/META-INF/MOJANG_C.SF" ]; then
+  rm "$minecraft/tmp/META-INF/MOJANG_C.SF"
+fi
+
+# Rejar it
+echo "4. Packing minecraft.jar back up"
+
+cd "$minecraft/bin"
+jar cf minecraft.jar -C ../tmp ../tmp/*
+
+# Remove MCDIR
+echo "5. Cleaning up..."
+rm -rf "$minecraft/tmp"
+
+echo "Completed"
